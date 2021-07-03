5edd1047b6dc6ced40845aeccc22e1a084e87903
furkant35 <furkant_35@hotmail.com>
2021-07-04 02:33:41 +0300

    RabbitMQ
    Basic RabbitMQ Project with asp.net core

---
+++ UdemyRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://inqqdvlv:twaufqgaU6gp-EczlbvBf2XNiqHquq5d@chimpanzee.rmq.cloudamqp.com/inqqdvlv")
+            };
+            // factory.HostName = "localhost"; 
+            using var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare("task_queue", durable: true, false, false, null);
+                channel.ExchangeDeclare("logs", durable: true, type: ExchangeType.Fanout);
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, false);
+                //Console.WriteLine("mesajları bekliyorum..");
+                Console.WriteLine("logları bekliyorum..");
+                var consumer = new EventingBasicConsumer(channel);
+
+
+
+                //channel.BasicConsume("task_queue", autoAck: false, consumer);
+                channel.BasicConsume(queueName, false, consumer);
+
+
+                consumer.Received += (model, ea) =>
+                {
+                    //var message = ea.Body;
+                    var log = ea.Body;
+                    //Console.WriteLine("Mesaj alındı: " + message);
+                    Console.WriteLine("Log alındı: " + log);
+                    int time = int.Parse(GetMessage(args));
+                    Thread.Sleep(time);
+                    //Console.WriteLine("Mesaj işlendi..");
+                    Console.WriteLine("Loglama bitti..");
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+            }
+            Console.WriteLine("Çıkış yapmak için tıklayınız");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://inqqdvlv:twaufqgaU6gp-EczlbvBf2XNiqHquq5d@chimpanzee.rmq.cloudamqp.com/inqqdvlv")
+            };
+            using var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare("task_queue", durable: true, false, false, null);
+                channel.ExchangeDeclare("logs", durable: true, type: ExchangeType.Fanout);
+                string message = GetMessage(args);
+                for (int i = 1; i < 11; i++)
+                {
+                    var bodyByte = Encoding.UTF8.GetBytes($"{message}-{i}");
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    //channel.BasicPublish("", routingKey: "task_queue", properties, body: bodyByte);
+                    channel.BasicPublish("logs", routingKey: "", properties, body: bodyByte);
+                    Console.WriteLine($"Mesajınız gönderilmiştir:{message}-{i}");
+                }
+            }
+            Console.WriteLine("Çıkış yapmak için tıklayınız");
+            Console.ReadLine();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
+        }
+    }
+}
